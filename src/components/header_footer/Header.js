c5e4df562af5537055471c5bc1d0b9fb8404bd12
linda eng <skyboxx@lindas-MacBook-Pro.local>
2019-07-11 10:52:00 -0400

    Rebel

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
 
@@ -18,9 +18,16 @@ class Header extends Component {
             }}>
             <Toolbar>
                 <div className="header_logo">
-                    <div></div> {/* add logo here */}
-                    <div className="header_logo_title">Dance Party</div> 
-
+                    <div><img src={require("../../resources/images/rebel_web_logo.png")} alt="" height="300" width="300"/></div> 
+                    <div >danceparty</div>
+                    <IconButton 
+                        aria-label="Menu"
+                        color="inherit"
+                        /*onClick={()=>}*/
+                    >
+                        
+                        <MenuIcon/>
+                    </IconButton>
                     
                 </div>
             </Toolbar> 
